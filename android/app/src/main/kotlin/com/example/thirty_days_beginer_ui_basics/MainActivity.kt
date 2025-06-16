2fc67005a8bf4ce67f9c70f9a2adac50fc4825a5
manhduydl <manhduydl@gmail.com>
2025-06-16 07:05:01 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/thirty_days_beginer_ui_basics/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.thirty_days_beginer_ui_basics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/day1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HorizontalListView extends StatefulWidget {
+  const HorizontalListView({super.key});
+
+  @override
+  State<HorizontalListView> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<HorizontalListView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
